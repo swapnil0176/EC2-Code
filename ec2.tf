93974c7a028499b2b098bdd8ba1d1d1abff495ee
abhijitn9 <abhijit.naikwadi@sorrisotech.com>
2026-06-23 20:54:44 +0530

    Added VPC module

--- ec2.tf
+++ ec2.tf
@@ -1,12 +1,108 @@
 provider "aws" {
   region = "us-west-2"
 }
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
 
-resource "aws_instance" "web" {
-  ami           = "ami-043ab4148b7bb33e9"
-  instance_type = "t3.micro"
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "us-west-2a"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Existing AWS Key Pair Name"
+  default = "PROD"
+}
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
   tags = {
-    Name = "my-ec2"
+    Name = "demo-vpc"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "demo-igw"
+  }
+}
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidr
+  availability_zone       = var.availability_zone
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet"
+  }
+}
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+resource "aws_security_group" "ec2_sg" {
+  name        = "ec2-security-group"
+  description = "Allow SSH"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2-sg"
+  }
+}
+resource "aws_instance" "web" {
+  ami                    = "ami-043ab4148b7bb33e9"
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+  key_name               = var.key_name
+
+  tags = {
+    Name = "demo-ec2"
   }
 }
