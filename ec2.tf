2f97b1e9a1cd1341d322662d6cf68f1be8328260
abhijitn9 <abhijit.naikwadi@sorrisotech.com>
2026-06-24 13:33:04 +0530

    added variables.tf file

--- ec2.tf
+++ ec2.tf
@@ -1,26 +1,7 @@
 provider "aws" {
   region = "us-west-2"
 }
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
 
-variable "public_subnet_cidr" {
-  default = "10.0.1.0/24"
-}
-
-variable "availability_zone" {
-  default = "us-west-2a"
-}
-
-variable "instance_type" {
-  default = "t3.micro"
-}
-
-variable "key_name" {
-  description = "Existing AWS Key Pair Name"
-  default = "PROD"
-}
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "us-west-2a"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Existing AWS Key Pair Name"
+  default = "PROD"
+}
