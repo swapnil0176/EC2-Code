fc363c8e898d6444cd5bc4056a99250f89855e60
swapnil0176 <swapniltawhare016@gmail.com>
2026-06-25 13:45:15 +0530

    Added all values in HCP

--- variables.tf
+++ variables.tf
@@ -1,21 +1,26 @@
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  type = string
+  #   default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidr" {
-  default = "10.0.1.0/24"
+  #   default = "10.0.1.0/24"
+  type = string
 }
 
 variable "availability_zone" {
-  default = "us-west-2a"
+  type = string
+  #   default = "us-west-2a"
 }
 
 variable "instance_type" {
-  default = "t3.micro"
+  type = string
+  #   default = "t3.micro"
 }
 
 variable "key_name" {
   description = "Existing AWS Key Pair Name"
-  default = "PROD"
+  type        = string
+  #   default = "PROD"
 }
 //Added comment
